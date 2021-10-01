f1ebc615d92ebd80b4545ce1d30f20b76a67d115
unknown <celia.faid95@gmail.com>
2021-09-30 22:07:23 -0400

    celia

---
+++ src/basic/LearnSwitchCase.java
@@ -0,0 +1,50 @@
+package basic;
+
+import javax.swing.*;
+
+public class LearnSwitchCase {
+    public static void main(String[] args) {
+      Scanner scan= new Scanner(System.in);
+        System.out.println("please enter your serial number ");
+          int foodSerial= scan.nextInt();
+         switch (foodSerial){
+             
+
+
+
+
+
+
+
+
+
+
+
+         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
